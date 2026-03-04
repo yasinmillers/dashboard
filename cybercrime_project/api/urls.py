22c65f787c7e38729be65742fe3066083f10c78c
yasinmillers <ssenyonga100@gmail.com>
2026-03-04 18:50:26 +0300

    views

--- cybercrime_project/api/urls.py
+++ cybercrime_project/api/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from .views import RegisterView, LoginView, ComplaintCreateView, ComplaintListView
+from .views import analytics
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
     path("complaint/", ComplaintCreateView.as_view(), name="complaint"),
     path("dashboard/", ComplaintListView.as_view(), name="dashboard"),
+     path("analytics/", analytics, name="analytics"),
 ]
+
+
