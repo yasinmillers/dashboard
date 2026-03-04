8bf13b8cf39a3538ac29752afab9a1a60808d4c8
yasinmillers <ssenyonga100@gmail.com>
2026-03-04 17:50:33 +0300

    analytcs

--- cybercrime_project/api/views.py
+++ cybercrime_project/api/views.py
@@ -15,3 +15,19 @@ def dashboard(request):
 def admin_dashboard(request):
     complaints = Complaint.objects.all()
     return render(request, "admin_dashboard.html", {"complaints": complaints})
+
+
+from django.db.models import Count
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import render
+from .models import Complaint
+
+@staff_member_required
+def analytics(request):
+    data = Complaint.objects.values('status').annotate(total=Count('id'))
+    total_complaints = Complaint.objects.count()
+
+    return render(request, "analytics.html", {
+        "data": data,
+        "total_complaints": total_complaints
+    })
