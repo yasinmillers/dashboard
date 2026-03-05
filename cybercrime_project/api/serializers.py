517fb822ffd42755b2e4ef374848f04f81668e90
yasinmillers <ssenyonga100@gmail.com>
2026-03-05 12:43:44 +0300

    role

--- cybercrime_project/api/serializers.py
+++ cybercrime_project/api/serializers.py
@@ -2,17 +2,19 @@ from rest_framework import serializers
 from .models import User, Complaint
 from django.contrib.auth import get_user_model
 
+
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
     class Meta:
-        model = get_user_model()
-        fields = ["id", "full_name", "email", "password"]
+        model = User
+        fields = ['id', 'username', 'password', 'role']
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        return get_user_model().objects.create_user(**validated_data)
+        user = User.objects.create_user(**validated_data)
+        return user
+
 
 class ComplaintSerializer(serializers.ModelSerializer):
     class Meta:
         model = Complaint
-        fields = ["complaint_id", "crime_type", "description", "status", "user"]
-        read_only_fields = ["complaint_id", "status", "user"]
+        fields = '__all__'
