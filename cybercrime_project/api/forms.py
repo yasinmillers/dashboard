d7091d85ce32f7aaad8d98fa676cdd4ef32f7222
yasinmillers <ssenyonga100@gmail.com>
2026-03-05 12:09:18 +0300

    forms

---
+++ cybercrime_project/api/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Complaint
+
+
+class ComplaintForm(forms.ModelForm):
+    class Meta:
+        model = Complaint
+        fields = ['title', 'description']
